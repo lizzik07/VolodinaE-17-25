22944052eea694f74bc379e40102cf51465808b6
lizzik07 <lzvolodina@mail.ru>
2026-09-24 17:12:30 +0300

    Update spaces_to_tabs.sh

--- pract1/spaces_to_tabs.sh
+++ pract1/spaces_to_tabs.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
-set -euo pipefail
-
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 input output" >&2
-    exit 1
-fi
-
-input="$1"
-output="$2"
-tab=$(printf '\t')
-
-sed "s/    /${tab}/g" "$input" > "$output"
+sed 's/    /\t/g' "$1" > "$2"
