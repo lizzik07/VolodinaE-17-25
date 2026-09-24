8504223358af455c03da1ea19e58c36f2f3af07f
lizzik07 <lzvolodina@mail.ru>
2026-09-24 17:13:59 +0300

    Задание 7

--- pract1/find_duplicates.sh
+++ pract1/find_duplicates.sh
@@ -1,24 +1,2 @@
 #!/bin/bash
-dir="${1:-.}"
-
-find "$dir" -type f -print0 \
-  | xargs -0 md5sum \
-  | sort \
-  | awk '
-      {
-        hash = $1
-        $1 = ""
-        file = substr($0, 2)
-        if (hash == prev_hash) {
-          if (!printed_group) {
-            print prev_file
-            printed_group = 1
-          }
-          print file
-        } else {
-          printed_group = 0
-        }
-        prev_hash = hash
-        prev_file = file
-      }
-    '
+find "${1:-.}" -type f -exec md5sum {} + | sort | uniq -w32 -D
