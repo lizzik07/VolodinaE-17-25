71a4a98e1e811e84eafec99ed9db2c8d905e5f66
lizzik07 <lzvolodina@mail.ru>
2026-09-24 17:14:28 +0300

    Задание 6

--- pract1/check_comments.sh
+++ pract1/check_comments.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
-dir="${1:-.}"
-
-find "$dir" -type f \( -name "*.c" -o -name "*.js" -o -name "*.py" \) | while read -r f; do
-    first_line=$(head -n 1 "$f")
-    case "$f" in
-        *.py)
-            if [[ "$first_line" == \#* ]]; then
-                echo "OK: $f"
-            else
-                echo "НЕТ КОММЕНТАРИЯ: $f"
-            fi
-            ;;
-        *.c|*.js)
-            if [[ "$first_line" == //* ]] || [[ "$first_line" == /\** ]]; then
-                echo "OK: $f"
-            else
-                echo "НЕТ КОММЕНТАРИЯ: $f"
-            fi
-            ;;
-    esac
+for f in *.c *.js *.py; do
+    head -n1 "$f" | grep -qE '^\s*(//|#|/\*)' \
+        && echo "OK: $f" \
+        || echo "НЕТ: $f"
 done
